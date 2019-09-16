f4705a18cccd17d7bccc9e518ba98ef2132a479c
saumendra08 <saumendra08@gmail.com>
2019-09-16 07:35:19 +0530

    added  comman

--- src/main/java/com/example/controller/Hellocontroller.java
+++ src/main/java/com/example/controller/Hellocontroller.java
@@ -1,11 +1,13 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="/hi")
 public class Hellocontroller {
+	@GetMapping(value="/hello")
 public String Hello() {
 	return "hi";
 }
